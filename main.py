af46316591819811923783de43efb47a937124df
vinnaf <vereslfanni@gmail.com>
2022-03-02 13:58:25 +0100

    Mai munkam

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+szam = str('1')
+
+for x in range(6):
+    print()
+    for y in range(6):
+        print(szam)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Fanni')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
